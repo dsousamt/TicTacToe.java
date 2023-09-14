77cc4116eea99431b972b2475a35e5fc114a861f
Matheus Sousa <dsousamt@gmail.com>
2023-09-14 10:18:13 -0300

    Fix a bug in GameController class

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -7,6 +7,7 @@ import entities.HumanPlayer;
 
 public class GameController {
 	
+	private Scanner sc = new Scanner(System.in);	
 	private HumanPlayer humanPlayer = new HumanPlayer();
 	private CPUPlayer cpuPlayer = new CPUPlayer();
 	private BoardController boardController = new BoardController();
@@ -15,35 +16,33 @@ public class GameController {
 	public GameController() {}
 
 	public void startGame() {
-		Scanner sc = new Scanner(System.in);		
-		boardController.showBoard();
 		
 		System.out.print("Qual o seu nome? ");
-		humanPlayer.setName(sc.nextLine());
+		humanPlayer.setName(this.sc.nextLine());
 		
 		System.out.print("Qual o nível de dificuldade desejado (Easy / Hard)? ");		
-		cpuPlayer.defineComputerLevel(sc.next());
+		cpuPlayer.defineComputerLevel(this.sc.next());
 		
 		System.out.print("A partida terá quantas rodadas? ");
-		for (int i = 1; i <= sc.nextInt(); i++) {
-			System.out.println("Rodada " + i + ", " + humanPlayer.getName());
-			this.newRound();
+		int nRound = sc.nextInt();
+		for (int i = 1; i <= nRound; i++) {
+			
+			while (!endGame) {
+				System.out.println("\nRodada " + i + ", " + humanPlayer.getName());
+				this.newRound();
+			}
 		}
-		
-		sc.close();
 	}
 	
 	public void newRound() {
-		Scanner sc = new Scanner(System.in);
 		boardController.showBoard();
 		System.out.print("Qual é a sua jogada (Coluna, Linha)? ");
-		humanPlayer.play(sc.nextInt(), sc.nextInt());
-		sc.skip("\\R");
+		humanPlayer.play(this.sc.nextInt(), this.sc.nextInt());
+		this.sc.skip("\\R");
 		
 		int xHuman = humanPlayer.getColumnValue();
 		int yHuman = humanPlayer.getRowValue();
 		if (boardController.isPossible(xHuman, yHuman)) {
-			System.out.println("é possivel Human");
 			boardController.changeBoard(yHuman, xHuman);
 		} else {
 			System.out.print("Jogada impossível");
@@ -61,16 +60,15 @@ public class GameController {
 		boardController.changeBoard(cpuPlayer.getColumnValue(), cpuPlayer.getRowValue());
 		
 		System.out.print("CPU jogou");
-		boardController.showBoard();
 		
 		if (boardController.verifyWin()) {
 			System.out.println("Houve um vencedor");
+			boardController.showBoard();
 			endGame = true;
 		} else if (boardController.verifyTie()) {
 			System.out.println("Empate - Ninguém ganhou");	
+			boardController.showBoard();
 			endGame = true;
 		}
-		
-		sc.close();
 	}
 }
