d09bcc9f7c64b3536d3490191125121cb1c7c34f
Matheus Sousa <dsousamt@gmail.com>
2023-09-14 09:19:31 -0300

    Add board coordinate's to BoardView

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -17,14 +17,30 @@ public class BoardView {
 	}
 	
 	public void show() {
+		
 		System.out.println();
+		
+		System.out.print("  ");
+		for (int column = 0; column < 3; column++) {
+			System.out.print(column + " ");
+		}
+		
+		System.out.println();
+		
+		int row = 0;
 		for (ArrayList<String> item : board.getBoard()) {
+			
+			System.out.print(row);				
+			
 			System.out.print("|");
 			for (String subItem : item) {
 				System.out.print(subItem + "|");
 			}
+			
+			row++;
 			System.out.println();
 		}
+		
 		System.out.println();
 	}
 }
